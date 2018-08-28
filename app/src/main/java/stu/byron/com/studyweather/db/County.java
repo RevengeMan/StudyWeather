7f5213a17abee3abe25ca9687a7b9360728f2c56
Byron <920687899@qq.com>
2018-08-28 16:03:03 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/stu/byron/com/studyweather/db/County.java
@@ -0,0 +1,46 @@
+package stu.byron.com.studyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Byron on 2018/8/28.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
